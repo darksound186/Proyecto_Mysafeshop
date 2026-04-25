cbad7400b8184e0f421956a851bcbc9f3be2c733
Darikson Cortes Developer Junior <dariksoc@gmail.com>
2026-04-25 16:01:47 +0000

    Carpetas front end

---
+++ My_Safe_shop/src/app/front/front-end/front-end.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-front-end',
+  imports: [],
+  templateUrl: './front-end.html',
+  styleUrl: './front-end.css',
+})
+export class FrontEnd {}
