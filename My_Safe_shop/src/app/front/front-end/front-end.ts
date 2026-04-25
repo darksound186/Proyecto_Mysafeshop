5f44687176fa869942bb18e880346407413d773d
Darikson Cortes Developer Junior <dariksoc@gmail.com>
2026-04-25 16:12:27 +0000

    instalacion dependencias y carpetas

--- My_Safe_shop/src/app/front/front-end/front-end.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-front-end',
-  imports: [],
-  templateUrl: './front-end.html',
-  styleUrl: './front-end.css',
-})
-export class FrontEnd {}
